c9b7c2bae281bb28a8b9db914dd9e04a8d68647e
VuDKT-Dev <vudkt.dev.hn@gmail.com>
2024-01-12 14:47:08 +0700

    project test

--- src/main/java/com/dienmayhoangtien_service/controller/DemoController.java
+++ src/main/java/com/dienmayhoangtien_service/controller/DemoController.java
@@ -15,7 +15,7 @@ public class DemoController {
     }
     @GetMapping
     public ResponseEntity<String> demo2(){
-        String result = "OK. CI11111111111!!!";
+        String result = "OK. CI11111111111111!!!";
         return ResponseEntity.ok(result);
     }
 }
